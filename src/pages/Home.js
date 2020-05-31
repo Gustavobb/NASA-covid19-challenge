18f3abe285b03c3807c25ac2de7775038b985396
CaioFauza <caiofauza@gmail.com>
2020-05-31 02:42:25 -0300

    Header + first section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grommet, Box, TextInput } from 'grommet';
+import { Grommet, Box, TextInput, Text, Header, Anchor, Image } from 'grommet';
 import { grommet } from 'grommet/themes';
 import { deepMerge } from 'grommet/utils';
 
@@ -16,10 +16,23 @@ class Home extends React.Component {
     render() {
         return (
             <Grommet theme={customTheme}>
-                <Box direction='row' pad='small'>
+                <Header background='#EDEDED' pad='small'>
+                    <Text textAlign='center' style={{ color: '#000000', fontSize: '2vh', letterSpacing: '1.5px' }} >Lockdown Hope</Text>
+                    <Box direction='row' gap='medium' style={{ marginRight: '2vw' }}>
+                        <Anchor color='#000000' label='Section 1' style={{ fontSize: '1.5vh', letterSpacing: '2px' }} />
+                        <Anchor color='#000000' label='Section 2' style={{ fontSize: '1.5vh', letterSpacing: '2px' }} />
+                    </Box>
+                </Header>
+                <Box background='#C6EC5B' direction='row' pad='xlarge' justify='center'>
+                    <Box direction='row' justify='start' gap='xlarge'>
+                        <Box direction='column' justify='center'>
+                            <Text textAlign='center' style={{ fontSize: '3.5vh', marginBottom: '2vh', letterSpacing: '1.5px' }}>Does quarantine affect the enviroment?</Text>
+                            <Text textAlign='center' style={{ fontSize: '1.5vh', letterSpacing: '1.5px' }}>The covid-19 pandemic changed several human activities.<br />
+                            Maybe for the environment this has a positive meaning</Text>
+                        </Box>
+                        <Image style={{ width: '25vw' }} src='https://picsum.photos/300/200' />
+                    </Box>
                 </Box>
-
-
             </Grommet>
         )
     }
