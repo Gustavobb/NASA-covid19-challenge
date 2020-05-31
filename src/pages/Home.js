629ece019ed96ad6dc623e7fb8e0964f9a0f5638
CaioFauza <caiofauza@gmail.com>
2020-05-31 00:09:18 -0300

    Folders structure initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component{
+    constructor(){
+
+    
+    }
+
+    render(){
+        return(
+            <div>A</div>
+        )
+    }
+}
+
+
+export default Home;
